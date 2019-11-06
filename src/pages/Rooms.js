9c291e8f65e3d759e6df167b0d9c74645bb23207
Dovydas <bumblysdovydas@gmail.com>
2019-11-06 23:29:02 +0200

    Added patient/doctor room assignment modals, moved styles to .css file

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -8,8 +8,10 @@ import Modal from "../components/Modal";
 
 import SiteWrapper from "../SiteWrapper.react";
 
+import "../styles/rooms.css";
 
-const roomsData = [
+
+let roomsData = [
     { Number: 1, Area: 49, TypeId: 1, TypeName: "Palata", Size: 6 },
     { Number: 2, Area: 11, TypeId: 2, TypeName: "Kabinetas", Size: 1 },
     { Number: 3, Area: 12, TypeId: 3, TypeName: "Procedūrinis k.", Size: 2 },
@@ -26,9 +28,17 @@ function RoomsPage() {
   const [showAdd, setShowAdd] = useState(false);
 
   const removeModals = [];
+  const patientModals = []
+  const doctorModals = [];
   roomsData.forEach(room => {
     const [showRemove, setShowRemove] = useState(false);
     removeModals[room.Number] = {showRemove, setShowRemove};
+    
+    const [showPatient, setShowPatient] = useState(false);
+    patientModals[room.Number] = {showPatient, setShowPatient};
+
+    const [showDoctor, setShowDoctor] = useState(false);
+    doctorModals[room.Number] = {showDoctor, setShowDoctor};
   });
   
   return (
@@ -38,14 +48,14 @@ function RoomsPage() {
           title="Patalpos"
         />
         
-      <div style={{display: 'flex', margin: '0em 0.7em 1em 0em'}}>
+      <div className="rooms-type-select">
         <Form.Select className="w-auto mr-2">
-            <option value="">Pasirinkite patalpos tipą</option>
+            <option value="aaa">Pasirinkite patalpos tipą</option>
             <option value="1">Palatos</option>
             <option value="2">Kabinetai</option>
             <option value="3">Procedūriniai kabinetai</option>
         </Form.Select>
-        <div style={{marginLeft: "auto"}}>
+        <div className="add-room-button">
             <Button color="primary" onClick={() => setShowAdd(true)}>+ Pridėti patalpą</Button>
             <Modal                //Add new room modal (pop-up)
               show={showAdd} 
@@ -110,17 +120,83 @@ function RoomsPage() {
                     <Table.Col alignContent="right">
                       <Button.List>
                         <Button color="info">Detalesnė informacija</Button>
-                        <Button color="gray" disabled={room.TypeId !== 1}>Priskirti pacientui</Button>
+
+                        <Button color="gray" disabled={room.TypeId !== 1} onClick={() => patientModals[room.Number].setShowPatient(true)}>
+                          Priskirti pacientui
+                        </Button>
+                        <Modal                //Assign room to patient (pop-up)
+                          show={patientModals[room.Number].showPatient} 
+                          handleClose={() => patientModals[room.Number].setShowPatient(false)} 
+                          title='Pridėti patalpą' 
+                          bodyContent={
+                            <Form>
+                              <Form.Label>Paciento vardas pavardė</Form.Label>
+                              <Form.Input
+                                className="mb-3"
+                                icon="search"
+                                placeholder="Įveskite paciento vardą ir pavardę..."
+                                position="append"
+                              />
+                              <Form.Label>Palata priskiriama nuo</Form.Label>
+                              <Form.DatePicker
+                               format="yyyy/mm/dd"
+                               monthLabels={["Sausio", "Vasario", "Kovo", "Balandžio", "Gegužės", "Birželio", "Liepos", "Rugpjūčio", "Rugsėjo", "Spalio", "Lapkričio", "Gruodžio"]}/>
+                              <Form.Label>Palata priskiriama iki</Form.Label>
+                              <Form.DatePicker
+                               format="yyyy/mm/dd"
+                               monthLabels={["Sausio", "Vasario", "Kovo", "Balandžio", "Gegužės", "Birželio", "Liepos", "Rugpjūčio", "Rugsėjo", "Spalio", "Lapkričio", "Gruodžio"]}/>
+                            </Form>
+                          }
+                          actions={[
+                              { label:"Atšaukti", onClick:() => patientModals[room.Number].setShowPatient(false) }, 
+                              { label:"Patvirtinti", color:"primary", onClick:() => patientModals[room.Number].setShowPatient(false) }
+                          ]}/>
+
                         <Button color="gray" disabled={room.TypeId !== 3}>Priskirti procedūrai</Button>
-                        <Button color="gray" disabled={room.TypeId !== 2 && room.TypeId !== 3}>Priskirti gydytojui</Button>
+
+                        <Button 
+                          color="gray" 
+                          disabled={room.TypeId !== 2 && room.TypeId !== 3} 
+                          onClick={() => doctorModals[room.Number].setShowDoctor(true)}>
+                          Priskirti gydytojui
+                        </Button>
+                        <Modal                //Assign room to doctor (pop-up)
+                          show={doctorModals[room.Number].showDoctor} 
+                          handleClose={() => doctorModals[room.Number].setShowDoctor(false)} 
+                          title='Pridėti patalpą' 
+                          bodyContent={
+                            <Form>
+                              <Form.Label>Gydytojo vardas pavardė</Form.Label>
+                              <Form.Input
+                                className="mb-3"
+                                icon="search"
+                                placeholder="Įveskite paciento vardą ir pavardę..."
+                                position="append"
+                              />
+                              <Form.Label>Kabinetas priskiriamas nuo</Form.Label>
+                              <Form.DatePicker
+                               format="yyyy/mm/dd"
+                               monthLabels={["Sausio", "Vasario", "Kovo", "Balandžio", "Gegužės", "Birželio", "Liepos", "Rugpjūčio", "Rugsėjo", "Spalio", "Lapkričio", "Gruodžio"]}/>
+                              <Form.Label>Kabinetas priskiriamas iki</Form.Label>
+                              <Form.DatePicker
+                               format="yyyy/mm/dd"
+                               monthLabels={["Sausio", "Vasario", "Kovo", "Balandžio", "Gegužės", "Birželio", "Liepos", "Rugpjūčio", "Rugsėjo", "Spalio", "Lapkričio", "Gruodžio"]}/>
+                            </Form>
+                          }
+                          actions={[
+                              { label:"Atšaukti", onClick:() => doctorModals[room.Number].setShowDoctor(false) }, 
+                              { label:"Patvirtinti", color:"primary", onClick:() => doctorModals[room.Number].setShowDoctor(false) }
+                          ]}/>
+
                         <Button color="warning">Redaguoti</Button>
+
                         <Button color="danger" onClick={() => removeModals[room.Number].setShowRemove(true)}>Šalinti</Button>
-                        <Modal
+                        <Modal      //remove room modal
                           show={removeModals[room.Number].showRemove} 
                           handleClose={() => removeModals[room.Number].setShowRemove(false)} 
                           title="Kabineto šalinimas"
                           bodyContent={
-                            <Text style={{fontSize: "initial", textAlign: "center"}}>
+                            <Text className="remove-room-text">
                               Ar tikrai norite pašalinti {room.Number} patalpą?
                             </Text>
                           }
