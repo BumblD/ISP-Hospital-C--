a250be9561f74058682208bc954e9592eee09e4c
deitam <dtamkus@gmail.com>
2019-12-16 02:05:45 +0200

    hopefully-the-end

--- back-end/HospitalBackEnd/HospitalBackEnd/Controllers/VartotojasController.cs
+++ back-end/HospitalBackEnd/HospitalBackEnd/Controllers/VartotojasController.cs
@@ -42,7 +42,11 @@ namespace HospitalBackEnd.Controllers
                                     kraujo_grupe.name as kraujo_grupe,
                                     pacientai.rezus as rezus,
                                     gydytojai.laipsnis as laipsnis,
-                                    vartotojo_info.id as id
+                                    vartotojo_info.id as id,
+                                    personalo_darbuotojai.tabelio_numeris as tabelio_numeris,
+                                    pacientai.fk_GYDYTOJASid as priskirtas,
+                                    gydytojai.id as gydytojas,
+                                    gydytojo_tipas.id as specializacijos_kodas
                                 FROM asmenine_info
                                     LEFT JOIN vartotojo_info ON asmenine_info.fk_VARTOTOJO_INFOid=vartotojo_info.id
                                     LEFT JOIN personalo_darbuotojai ON asmenine_info.asmens_kodas=personalo_darbuotojai.fk_ASMENINE_INFOasmens_kodas
@@ -67,15 +71,19 @@ namespace HospitalBackEnd.Controllers
                         Lytis = reader["lytis"] != DBNull.Value ? Convert.ToString(reader["lytis"]): null,
                         El_Pastas = reader["el_pastas"] != DBNull.Value ? Convert.ToString(reader["el_pastas"]): null,
                         Slaptazodis = reader["slaptazodis"] != DBNull.Value ? Convert.ToString(reader["slaptazodis"]): null,
-                        Tipas = reader["tipas"] != DBNull.Value ? Convert.ToInt32(reader["tipas"]): -1,
+                        Tipas = reader["tipas"] != DBNull.Value ? Convert.ToString(reader["tipas"]): null,
                         Tipo_Vardas = reader["tipo_vardas"] != DBNull.Value ? Convert.ToString(reader["tipo_vardas"]): null,
                         Specializacija = reader["specializacija"] != DBNull.Value ? Convert.ToString(reader["specializacija"]): null,
-                        Ugis = reader["ugis"] != DBNull.Value ? Convert.ToDouble(reader["ugis"]): -1,
-                        Mase = reader["mase"] != DBNull.Value ? Convert.ToDouble(reader["mase"]): -1,
+                        Ugis = reader["ugis"] != DBNull.Value ? Convert.ToString(reader["ugis"]): null,
+                        Mase = reader["mase"] != DBNull.Value ? Convert.ToString(reader["mase"]): null,
                         Kraujo_Grupe = reader["kraujo_grupe"] != DBNull.Value ? Convert.ToString(reader["kraujo_grupe"]): null,
-                        Rezus = reader["rezus"] != DBNull.Value ? Convert.ToInt32(reader["rezus"]): -1,
+                        Rezus = reader["rezus"] != DBNull.Value ? Convert.ToString(Convert.ToInt32(reader["rezus"])) : null,
                         Laipsnis = reader["laipsnis"] != DBNull.Value ? Convert.ToString(reader["laipsnis"]): null,
-                        Id = reader["id"] != DBNull.Value ? Convert.ToInt32(reader["id"]): -1,
+                        Id = reader["id"] != DBNull.Value ? Convert.ToString(reader["id"]): null,
+                        Tabelio_Numeris = reader["tabelio_numeris"] != DBNull.Value ? Convert.ToString(reader["tabelio_numeris"]): null,
+                        Priskirtas = reader["priskirtas"] != DBNull.Value ? Convert.ToString(reader["priskirtas"]): null,
+                        Gydytojas = reader["gydytojas"] != DBNull.Value ? Convert.ToString(reader["gydytojas"]): null,
+                        Specializacijos_Kodas = reader["specializacijos_kodas"] != DBNull.Value ? Convert.ToString(reader["specializacijos_kodas"]): null,
                     });
                 }
             }
@@ -107,7 +115,11 @@ namespace HospitalBackEnd.Controllers
                                     kraujo_grupe.name as kraujo_grupe,
                                     pacientai.rezus as rezus,
                                     gydytojai.laipsnis as laipsnis,
-                                    personalo_darbuotojai.tabelio_Numeris as tabelio_numeris
+                                    vartotojo_info.id as id,
+                                    personalo_darbuotojai.tabelio_numeris as tabelio_numeris,
+                                    pacientai.fk_GYDYTOJASid as priskirtas,
+                                    gydytojai.id as gydytojas,
+                                    gydytojo_tipas.id as specializacijos_kodas
                                 FROM asmenine_info
                                     LEFT JOIN vartotojo_info ON asmenine_info.fk_VARTOTOJO_INFOid=vartotojo_info.id
                                     LEFT JOIN personalo_darbuotojai ON asmenine_info.asmens_kodas=personalo_darbuotojai.fk_ASMENINE_INFOasmens_kodas
@@ -132,15 +144,19 @@ namespace HospitalBackEnd.Controllers
                         Lytis = reader["lytis"] != DBNull.Value ? Convert.ToString(reader["lytis"]): null,
                         El_Pastas = reader["el_pastas"] != DBNull.Value ? Convert.ToString(reader["el_pastas"]): null,
                         Slaptazodis = reader["slaptazodis"] != DBNull.Value ? Convert.ToString(reader["slaptazodis"]): null,
-                        Tipas = reader["tipas"] != DBNull.Value ? Convert.ToInt32(reader["tipas"]): -1,
+                        Tipas = reader["tipas"] != DBNull.Value ? Convert.ToString(reader["tipas"]): null,
                         Tipo_Vardas = reader["tipo_vardas"] != DBNull.Value ? Convert.ToString(reader["tipo_vardas"]): null,
                         Specializacija = reader["specializacija"] != DBNull.Value ? Convert.ToString(reader["specializacija"]): null,
-                        Ugis = reader["ugis"] != DBNull.Value ? Convert.ToDouble(reader["ugis"]): -1,
-                        Mase = reader["mase"] != DBNull.Value ? Convert.ToDouble(reader["mase"]): -1,
+                        Ugis = reader["ugis"] != DBNull.Value ? Convert.ToString(reader["ugis"]): null,
+                        Mase = reader["mase"] != DBNull.Value ? Convert.ToString(reader["mase"]): null,
                         Kraujo_Grupe = reader["kraujo_grupe"] != DBNull.Value ? Convert.ToString(reader["kraujo_grupe"]): null,
-                        Rezus = reader["rezus"] != DBNull.Value ? Convert.ToInt32(reader["rezus"]): -1,
+                        Rezus = reader["rezus"] != DBNull.Value ? Convert.ToString(Convert.ToInt32(reader["rezus"])) : null,
                         Laipsnis = reader["laipsnis"] != DBNull.Value ? Convert.ToString(reader["laipsnis"]): null,
-                        Tabelio_Numeris = reader["tabelio_numeris"] != DBNull.Value ? Convert.ToInt32(reader["tabelio_numeris"]): -1,
+                        Id = reader["id"] != DBNull.Value ? Convert.ToString(reader["id"]): null,
+                        Tabelio_Numeris = reader["tabelio_numeris"] != DBNull.Value ? Convert.ToString(reader["tabelio_numeris"]): null,
+                        Priskirtas = reader["priskirtas"] != DBNull.Value ? Convert.ToString(reader["priskirtas"]): null,
+                        Gydytojas = reader["gydytojas"] != DBNull.Value ? Convert.ToString(reader["gydytojas"]): null,
+                        Specializacijos_Kodas = reader["specializacijos_kodas"] != DBNull.Value ? Convert.ToString(reader["specializacijos_kodas"]): null,
                     };
                 }
                 return Json(userByID);
@@ -198,27 +214,27 @@ namespace HospitalBackEnd.Controllers
             
             return user.Tipas switch
             {
-                1 => CreateAdministrator(user),
-                2 => CreateDoctor(user),
-                3 => CreateLabWorker(user),
-                4 => CreatePatient(user),
+                "1" => CreateAdministrator(user),
+                "2" => CreateDoctor(user),
+                "3" => CreateLabWorker(user),
+                "4" => CreatePatient(user),
                 _ => BadRequest("Netinkamas arba nenurodytas vartotojo tipas.")
             };
         }
 
         public IActionResult CreatePatient(VartotojasViewModel user)
         {
-            if (user.Ugis <= 0)
+            if (user.Ugis == null)
             {
                 return BadRequest("Nenurodytas arba netinkamas ūgis.");
             }
             
-            if (user.Mase <= 0)
+            if (user.Mase == null)
             {
                 return BadRequest("Nenurodyta arba netinkama masė.");
             }
             
-            if (user.Rezus < 0)
+            if (user.Rezus == null)
             {
                 return BadRequest("Nenurodytas rezus.");
             }
@@ -289,7 +305,7 @@ namespace HospitalBackEnd.Controllers
                 return BadRequest("Nenurodytas laipsnis.");
             }
             
-            if (user.Specializacijos_kodas <= 0)
+            if (user.Specializacijos_Kodas == null)
             {
                 return BadRequest("Nenurodyta specializacija.");
             }
@@ -324,7 +340,7 @@ namespace HospitalBackEnd.Controllers
                 
                 cmd.CommandText = @"INSERT INTO gydytojai (laipsnis, specializacija, fk_PERSONALO_DARBUOTOJAStabelio_numeris) VALUES(@laipsnis, @specializacija, @fk_PERSONALO_DARBUOTOJAStabelio_numeris)";
                 cmd.Parameters.AddWithValue("@laipsnis", user.Laipsnis);
-                cmd.Parameters.AddWithValue("@specializacija", user.Specializacijos_kodas);
+                cmd.Parameters.AddWithValue("@specializacija", user.Specializacijos_Kodas);
                 cmd.Parameters.AddWithValue("@fk_PERSONALO_DARBUOTOJAStabelio_numeris", tab);
                 cmd.ExecuteNonQuery();
 
@@ -428,12 +444,7 @@ namespace HospitalBackEnd.Controllers
             {
                 return BadRequest("Nenurodyta pavardė.");
             }
-            
-            if (user.Gimimo_Data == null)
-            {
-                return BadRequest("Nenurodyta gimimo data.");
-            }
-            
+
             if (user.Telefonas == null)
             {
                 return BadRequest("Nenurodytas telefonas.");
@@ -446,16 +457,16 @@ namespace HospitalBackEnd.Controllers
             
             return user.Tipas switch
             {
-                1 => UpdateAdministrator(id, user),
-                2 => UpdateDoctor(id, user),
-                3 => UpdateLabWorker(id, user),
-                4 => UpdatePatient(id, user),
+                "1" => UpdateAdministrator(id, user),
+                "2" => UpdateDoctor(id, user),
+                "3" => UpdateLabWorker(id, user),
+                "4" => UpdatePatient(id, user),
                 _ => BadRequest("Netinkamas arba nenurodytas vartotojo tipas.")
             };
         }
         
         [HttpPut]
-        public IActionResult AssignDoctor([FromRoute] string id, [FromBody] int doctor)
+        public IActionResult AssignDoctor([FromRoute] string id, [FromBody] VartotojasViewModel doctor)
         {
             Db.Connection.Open();
             var cmd = Db.Connection.CreateCommand() as MySqlCommand;
@@ -463,7 +474,7 @@ namespace HospitalBackEnd.Controllers
             cmd.CommandText = @"UPDATE pacientai
                                     SET fk_GYDYTOJASid=@doctor
                                     WHERE fk_ASMENINE_INFOasmens_kodas=@id";
-            cmd.Parameters.AddWithValue("@doctor", doctor);
+            cmd.Parameters.AddWithValue("@doctor", doctor.Priskirtas);
             cmd.Parameters.AddWithValue("@id", id);
             cmd.ExecuteNonQuery();
             
@@ -472,17 +483,17 @@ namespace HospitalBackEnd.Controllers
 
         public IActionResult UpdatePatient(int user_id, VartotojasViewModel user)
         {
-            if (user.Ugis <= 0)
+            if (user.Ugis == null)
             {
                 return BadRequest("Nenurodytas arba netinkamas ūgis.");
             }
             
-            if (user.Mase <= 0)
+            if (user.Mase == null)
             {
                 return BadRequest("Nenurodyta arba netinkama masė.");
             }
             
-            if (user.Rezus < 0)
+            if (user.Rezus == null)
             {
                 return BadRequest("Nenurodytas rezus.");
             }
@@ -504,13 +515,11 @@ namespace HospitalBackEnd.Controllers
                 cmd.CommandText = @"UPDATE asmenine_info
                                     SET vardas=@vardas,
                                         pavarde=@pavarde,
-                                        gimimo_data=@gimimo_data,
                                         telefonas=@telefonas,
                                         lytis=@lytis
                                     WHERE fk_VARTOTOJO_INFOid=@fk_VARTOTOJO_INFOid";
                 cmd.Parameters.AddWithValue("@vardas", user.Vardas);
                 cmd.Parameters.AddWithValue("@pavarde", user.Pavarde);
-                cmd.Parameters.AddWithValue("@gimimo_data", user.Gimimo_Data);
                 cmd.Parameters.AddWithValue("@telefonas", user.Telefonas);
                 cmd.Parameters.AddWithValue("@lytis", user.Lytis.Equals("vyras") ? 1: 2);
                 cmd.Parameters.AddWithValue("@fk_VARTOTOJO_INFOid", user_id);
@@ -564,7 +573,7 @@ namespace HospitalBackEnd.Controllers
                 return BadRequest("Nenurodytas laipsnis.");
             }
             
-            if (user.Specializacijos_kodas <= 0)
+            if (user.Specializacijos_Kodas == null)
             {
                 return BadRequest("Nenurodyta specializacija.");
             }
@@ -585,13 +594,11 @@ namespace HospitalBackEnd.Controllers
                 cmd.CommandText = @"UPDATE asmenine_info
                                     SET vardas=@vardas,
                                         pavarde=@pavarde,
-                                        gimimo_data=@gimimo_data,
                                         telefonas=@telefonas,
                                         lytis=@lytis
                                     WHERE fk_VARTOTOJO_INFOid=@fk_VARTOTOJO_INFOid";
                 cmd.Parameters.AddWithValue("@vardas", user.Vardas);
                 cmd.Parameters.AddWithValue("@pavarde", user.Pavarde);
-                cmd.Parameters.AddWithValue("@gimimo_data", user.Gimimo_Data);
                 cmd.Parameters.AddWithValue("@telefonas", user.Telefonas);
                 cmd.Parameters.AddWithValue("@lytis", user.Lytis.Equals("vyras") ? 1: 2);
                 cmd.Parameters.AddWithValue("@fk_VARTOTOJO_INFOid", user_id);
@@ -603,7 +610,7 @@ namespace HospitalBackEnd.Controllers
                                     WHERE fk_PERSONALO_DARBUOTOJAStabelio_numeris=@fk_PERSONALO_DARBUOTOJAStabelio_numeris";
 
                 cmd.Parameters.AddWithValue("@laipsnis", user.Laipsnis);
-                cmd.Parameters.AddWithValue("@specializacija", user.Specializacijos_kodas);
+                cmd.Parameters.AddWithValue("@specializacija", user.Specializacijos_Kodas);
                 cmd.Parameters.AddWithValue("@fk_PERSONALO_DARBUOTOJAStabelio_numeris", user.Tabelio_Numeris);
                 cmd.ExecuteNonQuery();
 
@@ -633,13 +640,11 @@ namespace HospitalBackEnd.Controllers
                 cmd.CommandText = @"UPDATE asmenine_info
                                     SET vardas=@vardas,
                                         pavarde=@pavarde,
-                                        gimimo_data=@gimimo_data,
                                         telefonas=@telefonas,
                                         lytis=@lytis
                                     WHERE fk_VARTOTOJO_INFOid=@fk_VARTOTOJO_INFOid";
                 cmd.Parameters.AddWithValue("@vardas", user.Vardas);
                 cmd.Parameters.AddWithValue("@pavarde", user.Pavarde);
-                cmd.Parameters.AddWithValue("@gimimo_data", user.Gimimo_Data);
                 cmd.Parameters.AddWithValue("@telefonas", user.Telefonas);
                 cmd.Parameters.AddWithValue("@lytis", user.Lytis.Equals("vyras") ? 1: 2);
                 cmd.Parameters.AddWithValue("@fk_VARTOTOJO_INFOid", user_id);
@@ -671,13 +676,11 @@ namespace HospitalBackEnd.Controllers
                 cmd.CommandText = @"UPDATE asmenine_info
                                     SET vardas=@vardas,
                                         pavarde=@pavarde,
-                                        gimimo_data=@gimimo_data,
                                         telefonas=@telefonas,
                                         lytis=@lytis
                                     WHERE fk_VARTOTOJO_INFOid=@fk_VARTOTOJO_INFOid";
                 cmd.Parameters.AddWithValue("@vardas", user.Vardas);
                 cmd.Parameters.AddWithValue("@pavarde", user.Pavarde);
-                cmd.Parameters.AddWithValue("@gimimo_data", user.Gimimo_Data);
                 cmd.Parameters.AddWithValue("@telefonas", user.Telefonas);
                 cmd.Parameters.AddWithValue("@lytis", user.Lytis.Equals("vyras") ? 1: 2);
                 cmd.Parameters.AddWithValue("@fk_VARTOTOJO_INFOid", user_id);
